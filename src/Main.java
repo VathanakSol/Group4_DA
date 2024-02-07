43122bfde83a087625c515d860a8ff2457ae6e45
Vathanak <102912156+VathanakSol@users.noreply.github.com>
2024-02-07 13:06:33 +0000

    add new line of code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome for using JetBrain IDEs call Intellij");
+        System.out.println("This is a group 4");
     }
 }
