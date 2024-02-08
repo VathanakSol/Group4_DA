17d6742d2147be67ff5572e767c6aed7d901c3b8
Vathanak <vathanaksol1605@gmail.com>
2024-02-08 09:25:50 +0700

    Java I/O File Example of reading file (view)

---
+++ src/fileio/Main.java
@@ -0,0 +1,22 @@
+package fileio;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 1. Introduction to File I/O
+        // Here is an example show how to read file (view) from local drive
+        try{
+            FileReader fileReader = new FileReader("C:\\Users\\Kim Uy Computer\\Documents\\Textfile.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while((line = bufferedReader.readLine()) != null){
+                System.out.println(line);
+            }
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
