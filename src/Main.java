daf2c3c16217e38087db6849e7a796e15d53cabf
Vathanak <102912156+VathanakSol@users.noreply.github.com>
2024-02-13 07:26:06 +0000

    Add new line in Main File

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome for using JetBrain IDEs call Intellij");
         System.out.println("This is a group 4");
+        System.out.println("Git - Episode 3");
     }
 }
